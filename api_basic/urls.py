78b259d9eef88bc27fb98b8920fb70d89468dd8d
gautamamber <ambergautam1@gmail.com>
2020-02-15 13:23:51 +0530

    feat():add generic views and mixins

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import article_list, article_detail, ArticleAPIView, ArticleDetailView
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetailView, GenericAPIView
 
 urlpatterns = [
     path('articles/', article_list),
     path('article/<int:pk>', article_detail),
     path('articleapi/', ArticleAPIView.as_view()),
-    path('articleapi/<int:id>', ArticleDetailView.as_view())
+    path('articleapi/<int:id>', ArticleDetailView.as_view()),
+    path('generic/article/', GenericAPIView.as_view()),
+    path('generic/article/<int:id>/', GenericAPIView.as_view())
 ]
